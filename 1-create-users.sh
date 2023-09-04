2a59d37791036b569188f694e15141c18abccf33
addanuj <addanuj@gmail.com>
2023-09-04 20:43:31 +0530

    Update 1-create-users.sh

--- 1-create-users.sh
+++ 1-create-users.sh
@@ -5,7 +5,7 @@
 # Function to create a user
 create_user() {
   username=$1
-  password=$2
+  password="Workshop@123"
   useradd -G workshop $username
   echo -e "$password\n$password" | passwd $username &> /dev/null
   printf "| %-10s | %-15s |\n" $username $password
@@ -23,24 +23,21 @@ echo "1. Create Single User"
 echo "2. Create Multiple Users"
 read -p "Select an option (1/2): " option
 
+# Initialize table header
+printf "| %-10s | %-15s |\n" "Username" "Password"
+printf "|%11s|%16s|\n" "-----------" "----------------"
+
 case $option in
   1)
     read -p "Enter the username: " username
-    read -p "Enter the password: " password
-    # Print table header
-    printf "| %-10s | %-15s |\n" "Username" "Password"
-    printf "|%11s|%16s|\n" "-----------" "----------------"
-    create_user $username $password
+    create_user $username
     ;;
   2)
     read -p "Enter the number of users to create: " num_users
     read -p "Warning: This will create $num_users users with the password 'Workshop@123'. Do you want to continue? (y/n): " confirm
     if [ "$confirm" == "y" ]; then
-      # Print table header
-      printf "| %-10s | %-15s |\n" "Username" "Password"
-      printf "|%11s|%16s|\n" "-----------" "----------------"
       for i in $(seq 1 $num_users); do
-        create_user "user$i" "Workshop@123"
+        create_user "user$i"
       done
     else
       echo "User creation cancelled."
