311cadf87295dc27236c0e5144a5fc8bf7f7846b
addanuj <addanuj@gmail.com>
2023-09-04 21:02:02 +0530

    Update 1-create-users.sh

--- 1-create-users.sh
+++ 1-create-users.sh
@@ -17,9 +17,12 @@ create_user() {
 # Function to delete a user
 delete_user() {
   username=$1
+  if [ -z "$username" ]; then
+    echo "No username provided."
+    return
+  fi
   if [ "$username" != "root" ]; then
-    userdel -r $username
-    echo "User $username deleted."
+    userdel -r $username &> /dev/null && echo "User $username deleted." || echo "User $username could not be deleted."
   else
     echo "Cannot delete root user."
   fi
@@ -28,13 +31,22 @@ delete_user() {
 # Function to reset a user's password
 reset_password() {
   username=$1
+  if [ -z "$username" ]; then
+    echo "No username provided."
+    return
+  fi
   new_password="Workshop@123"
   if [ "$username" != "root" ]; then
-    echo -e "$new_password\n$new_password" | passwd $username &> /dev/null
-    echo "Password for $username reset to $new_password."
+    echo -e "$new_password\n$new_password" | passwd $username &> /dev/null && echo "Password for $username reset to $new_password." || echo "Could not reset password for $username."
   else
     echo "Cannot reset password for root user."
   fi
+}
+
+# Function to list all users
+list_users() {
+  echo "Listing all users:"
+  getent passwd | cut -d: -f1
 }
 
 # Create workshop group if it doesn't exist
@@ -49,15 +61,20 @@ echo "1. Create Single User"
 echo "2. Create Multiple Users"
 echo "3. Delete User"
 echo "4. Set Default Password for a user"
-read -p "Select an option (1/2/3/4): " option
+echo "5. List All Users"
+read -p "Select an option (1/2/3/4/5): " option
 
 case $option in
   1)
     read -p "Enter the username: " username
-    # Initialize table header
-    printf "| %-10s | %-15s |\n" "Username" "Password"
-    printf "|%11s|%16s|\n" "-----------" "----------------"
-    create_user $username
+    if [ -n "$username" ]; then
+      # Initialize table header
+      printf "| %-10s | %-15s |\n" "Username" "Password"
+      printf "|%11s|%16s|\n" "-----------" "----------------"
+      create_user $username
+    else
+      echo "No username provided. Exiting."
+    fi
     ;;
   2)
     read -p "Enter the number of users to create: " num_users
@@ -80,6 +97,9 @@ case $option in
   4)
     read -p "Enter the username for which to Set Default Password for a user: " username
     reset_password $username
+    ;;
+  5)
+    list_users
     ;;
   *)
     echo "Invalid option. Exiting."
